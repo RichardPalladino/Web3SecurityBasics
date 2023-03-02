01b193a79a5f056cddd5f2471063b661672e436d
RichardPalladino <rich@evolvedimprovement.com>
2023-03-01 17:41:06 -0800

    inspectBlock done

--- inspectBlock.js
+++ inspectBlock.js
@@ -7,34 +7,19 @@ const inspectBlock = async() => {
     const blockNum = await ethersProvider.getBlockNumber();
     console.log(`We are currently at ${blockNum} block.\nBlock info:\n`);
     const blockInfo = await ethersProvider.getBlock(blockNum);
-    console.log(blockInfo)
+    console.log(blockInfo);
 
-    // inspectTransactions(blockNum);
+    inspectTransactions(blockNum);
 
-    process.exit(1);
+    // process.exit(1);
 }
 
-const getBlockTransactions = async(_blockNum) => {
-    const results = await ethersProvider.getBlockWithTransactions(_blockNum);
-    return results;
+const inspectTransactions = async(blockNum) => {
+    const {transactions} = await ethersProvider.getBlockWithTransactions(blockNum);
+    transactions.forEach(function(transaction) {
+        console.log(transaction.hash);
+        console.log(transaction.to);
+    })
 }
 
-const inspectTransactions = async(block=0) => {
-    if(block == 0){
-        const blockNum = await ethersProvider.getBlockNumber();    
-        console.log(`Block #:  ${blockNum}\n`);
-        blockTransactions = await getBlockTransactions(blockNum);
-        console.log(blockTransactions);
-        process.exit(1);
-    }
-    else {
-        blockNum = block;
-        console.log(`Block #:  ${blockNum}\n`);
-        blockTransactions = await getBlockTransactions(blockNum);
-        console.log(blockTransactions);
-        return blockTransactions;
-    }
-}
-
-// inspectBlock();
-inspectTransactions();
+inspectBlock();
