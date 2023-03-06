de6e19694add6cce879a0d4695d72728353e85fc
RichardPalladino <rich@evolvedimprovement.com>
2023-03-06 14:50:06 -0800

    updated readme

--- web3ViewTransactions.js
+++ web3ViewTransactions.js
@@ -5,16 +5,16 @@ const w3 = new Web3(new Web3.providers.WebsocketProvider(WSSProvider));
 async function getTransaction(_txHash){
     let tx = await w3.eth.getTransaction(_txHash);
     if(tx != null){
-        // console.log(tx);
         console.log(`To: ${tx.to}`);
         console.log(`From: ${tx.from}`);
         console.log(`Value: ${tx.value}\n\n`);
         return tx;
     }
+    
+    // process.exit(1)
 }
 
 async function viewTransactions(){
-    // let subscription = 
     w3.eth.subscribe('pendingTransactions')
     .on("data", async function(transactionHash){
         await getTransaction(transactionHash);
